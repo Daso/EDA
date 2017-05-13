df1b1bd35c5e2aed181cba5c956f7508656485a8
Daniel Subauste Oliden <daalsuol@gmail.com>
2017-05-12 21:32:10 -0500

    Create EjemploNodos.rb

---
+++ Unidad01/Ejemplos/EjemploNodos.rb
@@ -0,0 +1,56 @@
+class Nodo
+  def initialize(value)
+    @value = value 
+    @next = nil 
+  end 
+  
+  def next
+    @next 
+  end 
+  
+  def next=(value)
+    @next = value 
+  end 
+end 
+
+class ListaEnlazada
+  def initialize
+    @head = @tail = nil 
+  end 
+  
+  def add(value)
+    nodo = Nodo.new(value)
+    @head = nodo if @head.nil? 
+    @tail.next = nodo unless @tail.nil?
+    @tail = nodo 
+  end 
+  
+  def removeAt(index)
+    prev = nil 
+    nodo = @head 
+    i = 0 
+    loop do 
+      prev = nodo 
+      nodo = nodo.next 
+      i += 1 
+      break unless !nodo.nil? and i < index
+    end
+    if prev.nil?
+      @head = nodo.next 
+    else
+      prev.next = nodo.next
+    end
+  end 
+end 
+
+lista = ListaEnlazada.new()
+lista.add("Casa")
+lista.add("Mama")
+lista.add("Abuela")
+puts lista.inspect
+lista.removeAt(1)
+puts " "
+puts lista.inspect
+
+
+
